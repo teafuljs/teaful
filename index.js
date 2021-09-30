bd8eb7738f55f7f2ac3266be5dfefd34702f0ca8
Aral Roca Gomez <aral-rg@hotmail.com>
2021-09-30 20:34:28 +0200

    Introduce callbacks

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-import React, { useState, useContext, createContext } from 'react'
+import React, { useState, useContext, createContext } from "react";
 
-export default function createStore(store = {}) {
-  const keys = Object.keys(store)
-  const capitalize = (k) => `${k[0].toUpperCase()}${k.slice(1, k.length)}`
+export default function createStore(store = {}, callbacks = {}) {
+  const keys = Object.keys(store);
+  const capitalize = (k) => `${k[0].toUpperCase()}${k.slice(1, k.length)}`;
 
   // storeUtils is the object we'll return with everything
   // (Provider, hooks)
@@ -10,11 +10,13 @@ export default function createStore(store = {}) {
   // We initialize it by creating a context for each property and
   // returning a hook to consume the context of each property
   const storeUtils = keys.reduce((o, key) => {
-    const context = createContext(store[key]) // Property context
-    const keyCapitalized = capitalize(key)
+    const context = createContext(store[key]); // Property context
+    const keyCapitalized = capitalize(key);
 
-    if (keyCapitalized === 'Store') {
-      console.error('Avoid to use the "store" name at the first level, it\'s reserved for the "useStore" hook.')
+    if (keyCapitalized === "Store") {
+      console.error(
+        'Avoid to use the "store" name at the first level, it\'s reserved for the "useStore" hook.'
+      );
     }
 
     return {
@@ -23,16 +25,29 @@ export default function createStore(store = {}) {
       contexts: [...(o.contexts || []), { context, key }],
       // Hook to consume the property context
       [`use${keyCapitalized}`]: () => useContext(context),
-    }
-  }, {})
+    };
+  }, {});
 
   // We create the main provider by wrapping all the providers
   storeUtils.Provider = ({ children }) => {
-    const Empty = ({ children }) => children
+    const Empty = ({ children }) => children;
     const Component = storeUtils.contexts
       .map(({ context, key }) => ({ children }) => {
-        const ctx = useState(store[key])
-        return <context.Provider value={ctx}>{children}</context.Provider>
+        const [value, setter] = useState(store[key]);
+        const cb = callbacks[key];
+
+        const updater = (newValue) => {
+          let nVal = newValue;
+          setter(newValue);
+          if (typeof nVal === "function") nVal = newValue(value);
+          if (typeof cb === "function") cb(nVal, value, setter);
+        };
+
+        return (
+          <context.Provider value={[value, updater]}>
+            {children}
+          </context.Provider>
+        );
       })
       .reduce(
         (RestProviders, Provider) =>
@@ -43,34 +58,34 @@ export default function createStore(store = {}) {
             </Provider>
           ),
         Empty
-      )
+      );
 
-    return <Component>{children}</Component>
-  }
+    return <Component>{children}</Component>;
+  };
 
   // As a bonus, we create the useStore hook to return all the
   // state. Also to return an updater that uses all the created hooks at
   // the same time
   storeUtils.useStore = () => {
-    const state = {}
-    const updates = {}
+    const state = {};
+    const updates = {};
     keys.forEach((k) => {
-      const hookName = `use${capitalize(k)}`
-      if (hookName === 'useStore') return
-      const [s, u] = storeUtils[hookName]()
-      state[k] = s
-      updates[k] = u
-    })
+      const hookName = `use${capitalize(k)}`;
+      if (hookName === "useStore") return;
+      const [s, u] = storeUtils[hookName]();
+      state[k] = s;
+      updates[k] = u;
+    });
 
     function updater(newState) {
       const s =
-        typeof newState === 'function' ? newState(state) : newState || {}
-      Object.keys(s).forEach((k) => updates[k] && updates[k](s[k]))
+        typeof newState === "function" ? newState(state) : newState || {};
+      Object.keys(s).forEach((k) => updates[k] && updates[k](s[k]));
     }
 
-    return [state, updater]
-  }
+    return [state, updater];
+  };
 
   // Return everything we've generated
-  return storeUtils
+  return storeUtils;
 }

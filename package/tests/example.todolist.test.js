5b79aba10dcb75a647b8ba3ce87a82e600f01958
Aral Roca Gomez <aral-rg@hotmail.com>
2021-10-27 23:35:08 +0200

    Improve test

--- package/tests/example.todolist.test.js
+++ package/tests/example.todolist.test.js
@@ -8,6 +8,8 @@ import createStore from '../index';
 describe('Example: Todo list', () => {
   it('should work with a simple todo list app', () => {
     const {useStore} = createStore({todo: [], done: []});
+    const renderAddTodoTask = jest.fn();
+    const renderTodoList = jest.fn();
 
     function AddTodoTask() {
       const [todo, setTodo] = useStore.todo();
@@ -15,8 +17,9 @@ describe('Example: Todo list', () => {
         e.preventDefault();
         setTodo([...todo, e.target.children[0].value]);
       };
+      renderAddTodoTask();
       return (
-        <form onSubmit={addTask}>
+        <form key={todo.length} onSubmit={addTask}>
           <input type="text" />
           <button type="submit">Add</button>
         </form>
@@ -24,8 +27,16 @@ describe('Example: Todo list', () => {
     }
 
     function TodoList() {
-      const [todo, setTodo] = useStore.todo();
+      const [todo, setTodo, resetTodo] = useStore.todo();
       const [done, setDone] = useStore.done();
+
+      function reset() {
+        resetTodo(); // Reset with initial value
+        setDone([]); // Reset with an empty array
+      }
+
+      renderTodoList();
+
       return (
         <div>
           <h1>Todo tasks</h1>
@@ -35,7 +46,7 @@ describe('Example: Todo list', () => {
                 {t}
                 <button
                   onClick={() => {
-                    setTodo(todo.filter((t) => t !== t));
+                    setTodo(todo.filter((i) => i !== t));
                     setDone([...done, t]);
                   }}
                 >
@@ -60,6 +71,7 @@ describe('Example: Todo list', () => {
               </li>
             ))}
           </ul>
+          <button onClick={reset}>Reset</button>
         </div>
       );
     }
@@ -71,20 +83,56 @@ describe('Example: Todo list', () => {
         </>,
     );
 
+    expect(renderAddTodoTask).toHaveBeenCalledTimes(1);
+    expect(renderTodoList).toHaveBeenCalledTimes(1);
+
     // Add task
+    userEvent.clear(screen.getByRole('textbox'));
     userEvent.type(screen.getByRole('textbox'), 'New task');
     userEvent.click(screen.getByText('Add'));
     expect(screen.getByTestId('todo').textContent).toContain('New task');
     expect(screen.getByTestId('done').textContent).not.toContain('New task');
+    expect(renderAddTodoTask).toHaveBeenCalledTimes(2);
+    expect(renderTodoList).toHaveBeenCalledTimes(2);
 
     // Move to done
     userEvent.click(screen.getByText('Done'));
     expect(screen.getByTestId('todo').textContent).not.toContain('New task');
     expect(screen.getByTestId('done').textContent).toContain('New task');
+    expect(renderAddTodoTask).toHaveBeenCalledTimes(3);
+    expect(renderTodoList).toHaveBeenCalledTimes(3);
 
     // Move to todo
     userEvent.click(screen.getByText('Undone'));
     expect(screen.getByTestId('todo').textContent).toContain('New task');
     expect(screen.getByTestId('done').textContent).not.toContain('New task');
+    expect(renderAddTodoTask).toHaveBeenCalledTimes(4);
+    expect(renderTodoList).toHaveBeenCalledTimes(4);
+
+    // Move again to done
+    userEvent.click(screen.getByText('Done'));
+    expect(screen.getByTestId('todo').textContent).not.toContain('New task');
+    expect(screen.getByTestId('done').textContent).toContain('New task');
+    expect(renderAddTodoTask).toHaveBeenCalledTimes(5);
+    expect(renderTodoList).toHaveBeenCalledTimes(5);
+
+    // Add another task
+    userEvent.type(screen.getByRole('textbox'), 'Another task');
+    userEvent.click(screen.getByText('Add'));
+    expect(screen.getByTestId('todo').textContent).toContain('Another task');
+    expect(screen.getByTestId('todo').textContent).not.toContain('New task');
+    expect(screen.getByTestId('done').textContent).toContain('New task');
+    expect(screen.getByTestId('done').textContent).not.toContain('Another task');
+    expect(renderAddTodoTask).toHaveBeenCalledTimes(6);
+    expect(renderTodoList).toHaveBeenCalledTimes(6);
+
+    // Reset
+    userEvent.click(screen.getByText('Reset'));
+    expect(screen.getByTestId('todo').textContent).not.toContain('New task');
+    expect(screen.getByTestId('todo').textContent).not.toContain('Another task');
+    expect(screen.getByTestId('done').textContent).not.toContain('New task');
+    expect(screen.getByTestId('done').textContent).not.toContain('Another task');
+    expect(renderAddTodoTask).toHaveBeenCalledTimes(7);
+    expect(renderTodoList).toHaveBeenCalledTimes(7);
   });
 });

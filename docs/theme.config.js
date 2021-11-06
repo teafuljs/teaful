231174ee6f01ba5fcca46a68b3b6f700ccb74865
Siddharth Borderwala <siddharthborderwala@gmail.com>
2021-11-06 14:55:32 +0530

    update to teaful
    add Input Params and Return Value sub-heading

--- docs/theme.config.js
+++ docs/theme.config.js
@@ -1,16 +1,17 @@
 export default {
-  github: 'https://github.com/aralroca/fragstore',
-  docsRepositoryBase: 'https://github.com/aralroca/fragstore/blob/master/docs',
-  titleSuffix: ' | Fragstore',
+  github: 'https://github.com/teafuljs/teaful',
+  docsRepositoryBase: 'https://github.com/teafuljs/teaful/',
+  titleSuffix: ' | Teaful',
   logo: (
     <>
-      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
-        strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-        <polygon points="12 2 2 7 12 12 22 7 12 2"></polygon>
-        <polyline points="2 17 12 22 22 17"></polyline>
-        <polyline points="2 12 12 17 22 12"></polyline>
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+        <path d="M18 8h1a4 4 0 0 1 0 8h-1"></path>
+        <path d="M2 8h16v9a4 4 0 0 1-4 4H6a4 4 0 0 1-4-4V8z"></path>
+        <line x1="6" y1="1" x2="6" y2="4"></line>
+        <line x1="10" y1="1" x2="10" y2="4"></line>
+        <line x1="14" y1="1" x2="14" y2="4"></line>
       </svg>
-      <span className="ml-2 font-extrabold hidden md:inline">Fragstore</span>
+      <span className="ml-2 font-extrabold hidden md:inline">Teaful</span>
     </>
   ),
   head: (
@@ -25,8 +26,8 @@ export default {
   nextLinks: true,
   footer: true,
   footerEditLink: 'Edit this page on GitHub',
-  footerText: <>&copy; {new Date().getFullYear()} Fragstore Contributors.</>,
-  unstable_faviconGlyph: '👾',
+  footerText: <>&copy; {new Date().getFullYear()} Teaful Contributors.</>,
+  unstable_faviconGlyph: '🍵',
   unstable_staticImage: true,
   font: false,
 }

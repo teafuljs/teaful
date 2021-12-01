da57c56e5610d02a850ffb6e9d49c510b177c6a0
Siddharth Borderwala <siddharthborderwala@gmail.com>
2021-12-01 19:28:39 +0530

    update docs

--- docs/components/feedback-rating.jsx
+++ docs/components/feedback-rating.jsx
@@ -39,11 +39,11 @@ const FeedbackRating = () => {
   return (
     <div style={{ width: '100%', display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center', marginTop: '2rem' }}>
       <h3>Was this helpful?</h3>
-      <div style={{ marginTop: '1rem', color: '#454545' }}>
-        <button style={{ marginRight: '1rem' }} onClick={sendNo} title="No">
+      <div style={{ marginTop: '1.5rem', color: '#454545' }}>
+        <button onClick={sendNo} title="No">
           <svg aria-label="No" xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="10"></circle><path d="M16 16s-1.5-2-4-2-4 2-4 2"></path><line x1="9" y1="9" x2="9.01" y2="9"></line><line x1="15" y1="9" x2="15.01" y2="9"></line></svg>
         </button>
-        <button style={{ marginRight: '1rem' }} onClick={sendMaybe} title="Maybe">
+        <button style={{ margin: '0 1.25rem' }} onClick={sendMaybe} title="Maybe">
           <svg aria-label="Maybe" xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="10"></circle><line x1="8" y1="15" x2="16" y2="15"></line><line x1="9" y1="9" x2="9.01" y2="9"></line><line x1="15" y1="9" x2="15.01" y2="9"></line></svg>
         </button>
         <button onClick={sendYes} title="Yes">

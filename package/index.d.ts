b70f074276c4ce0d68a612c503826b9fcf81f2ca
Danielo Artola <danielofair@gmail.com>
2021-12-02 16:24:32 +0100

    adding types order, moved types to package (#52)
    * adding types order, moved types to package
    
    * stop autogenerating types with microbuble with --no-generateTypes flag
    
    * Update package.json
    
    Co-authored-by: Aral Roca Gomez <aral-rg@hotmail.com>
    
    * put initialState as optional at createStore
    
    * upgrade canary version
    
    * set now accepts function as param
    
    * fix param on function as set value
    
    * remove type for reset on hookreturn
    
    * update canary version
    
    * update canary version
    
    Co-authored-by: Aral Roca Gomez <aral-rg@hotmail.com>

---
+++ package/index.d.ts
@@ -0,0 +1,53 @@
+declare module "teaful" {
+
+  import React from "react";
+  
+  type HookReturn<T> = [T, (value: T | ((value: T) => T | undefined | null) ) => void];
+  type initialStoreType = Record<string, any>;
+
+  type Hook<S> = (
+    initial?: S,
+    onAfterUpdate?: afterCallbackType<S>
+  ) => HookReturn<S>;
+
+  type HookDry<S> = (initial?: S) => HookReturn<S>;
+
+  export type Hoc<S> = { store: HookReturn<S> };
+
+  type HocFunc<S, R extends React.ComponentClass = React.ComponentClass> = (
+    component: R,
+    initial?: S
+  ) => R;
+
+  type afterCallbackType<S extends initialStoreType> = (param: {
+    store: S;
+    prevStore: S;
+  }) => void;
+
+  type getStoreType<S extends initialStoreType> = {
+    [key in keyof S | string ]: S[key] extends initialStoreType
+      ? useStoreType<S[key]> & HookDry<S[key]> : HookDry<S[key]>;
+  };
+
+  type useStoreType<S extends initialStoreType> = {
+    [key in keyof S]: S[key] extends initialStoreType
+      ? useStoreType<S[key]> & Hook<S[key]> : Hook<S[key]>;
+  };
+
+  type withStoreType<S extends initialStoreType> = {
+    [key in keyof S]: S[key] extends initialStoreType
+      ? withStoreType<S[key]> & HocFunc<S>
+      : HocFunc<S>;
+  };
+
+  function createStore<S extends initialStoreType>(
+    initial?: S,
+    afterCallback?: afterCallbackType<S>
+  ): {
+    getStore: HookDry<S> & getStoreType<S>;
+    useStore: Hook<S> & useStoreType<S>;
+    withStore: HocFunc<S> & withStoreType<S>;
+  };
+
+  export default createStore;
+}
